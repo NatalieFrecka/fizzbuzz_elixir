f5421e6e0c0c7daa4be70dd553695f6a5a6212d1
NatalieFrecka <nfrecka@gmail.com>
2019-09-11 04:48:22 -0500

    custom guards

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,7 +1,12 @@
 defmodule Fizzbuzz do
-  def fizzbuzz(input) when rem(input, 15) == 0, do: "FizzBuzz"
-  def fizzbuzz(input) when rem(input, 3) == 0, do: "Fizz"
-  def fizzbuzz(input) when rem(input, 5) == 0, do: "Buzz"
+  defguardp is_fizz(input) when rem(input, 3) == 0
+  defguardp is_buzz(input) when rem(input, 5) == 0
+  defguardp is_fizzbuzz(input) when is_fizz(input) and is_buzz(input)
+
+  def fizzbuzz(input) when is_fizzbuzz(input), do: "FizzBuzz"
+  def fizzbuzz(input) when is_fizz(input), do: "Fizz"
+  def fizzbuzz(input) when is_buzz(input), do: "Buzz"
   def fizzbuzz(input) when is_integer(input), do: input
   def fizzbuzz(_), do: "NAN"
+
 end
