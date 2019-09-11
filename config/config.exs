ea85f9f22de4fa4610d56f62a7f2679145ad0f88
NatalieFrecka <nfrecka@gmail.com>
2019-09-11 05:37:48 -0500

    using macros

---
+++ config/config.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :fizzbuzz, my_range: Enum.to_list 1..15
--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,5 +1,9 @@
 defmodule Fizzbuzz.Guards do
-  defguardp in_range(input) when input in 1..15
+  defmacrop in_range(input) do
+    my_range = Application.get_env(:fizzbuzz, :my_range)
+    quote do: unquote(input) in unquote(my_range)
+  end
+
   defguard is_fizz(input) when rem(input, 3) == 0 and in_range(input)
   defguard is_buzz(input) when rem(input, 5) == 0 and in_range(input)
   defguard is_fizzbuzz(input) when is_fizz(input) and is_buzz(input)
@@ -9,7 +13,6 @@ end
 
 defmodule Fizzbuzz do
   import Fizzbuzz.Guards
-
   def fizzbuzz(input) when is_fizzbuzz(input), do: "FizzBuzz"
   def fizzbuzz(input) when is_fizz(input), do: "Fizz"
   def fizzbuzz(input) when is_buzz(input), do: "Buzz"
